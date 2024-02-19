cc5ac2feb7f9685379b67ca114c89fe48ca9449c
Pavel Shyliahau <pashxa@syntronic.com>
2024-02-19 01:19:36 -0500

    FIrst try Of GHA

--- client/index.ts
+++ client/index.ts
@@ -19,6 +19,22 @@ app.get('/start', (req: Request, res: Response) => {
   }
 })
 
+app.get('/wallet', (req: Request, res: Response) => {
+  if (bot.isStarted()) {
+    res.send(`Bot is started. Current wallet:\n${JSON.stringify(bot.getWalletTradingInfo())}`)
+  } else {
+    res.send(`Bot is not started. Current wallet:\n${JSON.stringify(bot.getWalletTradingInfo())}`)
+  }
+})
+
+app.get('/skipped', (req: Request, res: Response) => {
+  res.send(JSON.stringify(bot.getSkippedPools()))
+})
+
+app.get('/trades', (req: Request, res: Response) => {
+  res.send(JSON.stringify(bot.getTradingResults()))
+})
+
 // Start the server
 app.listen(config.appPort, () => {
   console.log(`Server running on http://localhost:${config.appPort}`);
